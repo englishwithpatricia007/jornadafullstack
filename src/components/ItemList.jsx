8f24582465b33f032b33771446b76b6167569f3a
Patricia <englishwithpatricia007@gmail.com>
2025-05-07 09:42:10 -0300

    Usando chave fenda já que não tem a philips

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,6 +1,46 @@
 import React from "react";
+import SingleItem from "./SingleItem";
 
-const ItemList = () => {
+const ItemList = ({ title, items }) => {
+
+    return (
+        <div className="item-list">
+            <div className="item-list__header">
+                <h2>{title} populares</h2>
+                <a className="item-list__link"
+                    href="/">Mostrar tudo
+                </a>
+            </div>
+
+            <div className="item-list__container">
+                {items === 5 ? (
+                    <>
+                        <SingleItem />
+                        <SingleItem />
+                        <SingleItem />
+                        <SingleItem />
+                        <SingleItem />
+                    </>
+
+                ) : (
+                    <>
+                        <SingleItem />
+                        <SingleItem />
+                        <SingleItem />
+                        <SingleItem />
+                        <SingleItem />
+                        <SingleItem />
+                        <SingleItem />
+                        <SingleItem />
+                        <SingleItem />
+                        <SingleItem />
+                    </>
+                )
+                }
+
+            </div>
+        </div>
+    )
 
 }
 
--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,6 +1,30 @@
 import React from "react";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SingleItem = () => {
+    return (
+        <div className="single-item">
+            <div className="single-item__div-image-button">
+                <div className="single-item__div-image">
+                    <img className="single-item__image" src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                        alt="Imagem do artista Henrique & Juliano"
+                    />
+                </div>
+                <FontAwesomeIcon
+                    className="single-item__icon"
+                    icon={faCirclePlay}
+                />
+            </div>
+
+            <div className="single-item__texts">
+                <div className="sing-item__2lines">
+                    <p className="single-item__title">Henrique & Juliano</p>
+                    <p className="single-item__type">Artista</p>
+                </div>
+            </div>
+        </div>
+    )
 
 }
 
