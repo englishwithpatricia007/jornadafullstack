0a5ff7215ec2bc4557de515029f647e745d46300
Patricia <englishwithpatricia007@gmail.com>
2025-05-09 13:00:59 -0300

    lendo as tabelas do banco

--- backend/api/connect.js
+++ backend/api/connect.js
@@ -5,7 +5,9 @@ const URI ="mongodb+srv://glitchpsn007:866R@cluster0.lokqufz.mongodb.net/?retryW
 const client = new MongoClient(URI);
 
 
-const db = client.db("spotifyAula");
+export const db = client.db("spotifyAula");
 const songCollection = await db.collection('songs').find({}).toArray(); //JavaScript Assíncrono await async
-
 console.log(songCollection);
+
+const artistCollection = await db.collection('artists').find({}).toArray(); //JavaScript Assíncrono await async
+console.log(artistCollection);
--- backend/api/server.js
+++ backend/api/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import {artistsArray} from "../../frontend/src/assets/database/artists.js"
 import {songsArray} from "../../frontend/src/assets/database/songs.js"
+import { db } from './connect.js';
 
 
 const app = express();
@@ -14,10 +15,11 @@ app.get("/", (req, res) => {
     res.send("Olá, mundão! Be safe!")
 })
 
-app.get("/artists", (req, res) => {
-    res.send(artistsArray)
+app.get("/artists", async (req, res) => {
+    res.send(await db.collection('artists').find({}).toArray()); //JavaScript Assíncrono await async
 })
 
-app.get("/songs", (req, res) => {
-    res.send(songsArray)
+app.get("/songs", async (req, res) => {
+    res.send(await db.collection('songs').find({}).toArray())
 })
+
