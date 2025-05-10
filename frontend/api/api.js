adcd3b2a507de1c4225757aaaf5732b7a48cb4db
Patricia <englishwithpatricia007@gmail.com>
2025-05-09 21:42:06 -0300

    mais ajustes

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -2,13 +2,8 @@
 //Fetch ou Axios
 //Fetch é nativo do JavaScript
 //Axios é um pacote
-import "dotenv/config"
 import axios from "axios"
 
-//const { NODE_ENV } = process.env
-
-
-//const API_URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api"
 const API_URL =  "https://jornadafullstack-6jz5.onrender.com/api"
 
 const responseArtists = await axios.get(`${API_URL}/artists`);
